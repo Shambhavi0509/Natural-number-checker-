76bb2df958eb54c2134bf1f716aa960a2793240b
Shambhavi0509 <shampri596@gmail.com>
2025-06-14 07:46:39 +0530

    Update NaturalNumber.java
    Formatted code with proper indentation and spacing

--- NaturalNumber.java
+++ NaturalNumber.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
-class NaturalNumber
-{
-public static void main(String[] args)
-{
-int number, n;
-int sum = 0;
-Scanner input = new Scanner(System.in);
-// get a number from user
-System.out.print("Enter number to check if it is whole number or not");
-number = input.nextInt();
-if(number>0)
-{
-System.out.println("The number is a natural number");
+
+class NaturalNumber {
+    public static void main(String[] args) {
+        int number, n;
+        int sum = 0;
+        Scanner input = new Scanner(System.in);
+
+        // get a number from user
+        System.out.print("Enter number to check if it is whole number or not: ");
+        number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is a natural number");
+        } else {
+            System.out.println("It is not a natural number");
+        }
+
+        System.out.print("Enter the value for n: ");
+        n = input.nextInt();
+
+        for (int a = 1; a <= n; a++) {
+            sum = sum + a;
+        }
+
+        System.out.println("The sum of numbers is: " + sum);
+        input.close();
+    }
 }
-else
-{
-System.out.println("It is a not a natural number");
-}
-System.out.print("enter the value for n:");
-n = input.nextInt();
-for(int a = 1; a<=n; a++)
-{
-sum = sum + a;
-}
-System.out.println("the sum of numbers is:" + sum);
-input.close();
-}}
